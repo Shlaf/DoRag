f1ee0d54481d88f086986e632e6cbeb6b8d2d112
demirovic84 <demirovic84@gmail.com>
2017-08-23 13:17:10 +0200

    Buttons
    Alexander fixade buttons

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Button from "./components/button";
 import Addition from "./functions/addition";
 import Subtract from "./functions/subtract";
 import Multiply from "./functions/multiply";
@@ -7,13 +8,24 @@ import Divide from "./functions/divide";
 import './App.css';
 
 class App extends Component {
+  state = {
+    input: ""
+  }
+
+  handleInputClick = (input) => {
+    this.setState({input})
+  }
+
   render() {
     return (
       <main>
       <div className="calculatorz">
         <form>
-          <input type="text" name="value" id="value"/>
+          <input type="text" name="value" id="value" value={this.state.input}/>
         </form>
+        <div>
+          {[1, 2, 3].map((int) => <Button key={int} onClick={this.handleInputClick}>{int}</Button>)}
+        </div>
         </div>
       </main> 
     );
--- src/components/button.js
+++ src/components/button.js
@@ -1,1 +1,15 @@
-<div className=""
+import React, { Component } from 'react';
+
+class Button extends Component {
+  handleClick = () => {
+      this.props.onClick(this.props.children)
+  }  
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.props.children}
+      </button>
+    );
+  }
+}
+export default Button;
