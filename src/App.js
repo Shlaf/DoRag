d2d35016a3076eaa12120ac9b520c6aba1767b5a
Shlaf <ivanlundberg93@gmail.com>
2017-08-23 13:21:18 +0200

    added numbers

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
           <input type="text" name="value" id="value" value={this.state.input}/>
         </form>
         <div>
-          {[1, 2, 3].map((int) => <Button key={int} onClick={this.handleInputClick}>{int}</Button>)}
+          {[1, 2, 3, 4, 5, 6, 7 ,8, 9, 0].map((int) => <Button key={int} onClick={this.handleInputClick}>{int}</Button>)}
         </div>
         </div>
       </main> 
