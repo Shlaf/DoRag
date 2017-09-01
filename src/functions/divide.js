c1c268008746c530f6ae0a4de214d9ac072c111b
andersrochester <anders.rochester@gmail.com>
2017-09-01 12:19:19 +0200

    Changed to correct function name in functions

--- src/functions/divide.js
+++ src/functions/divide.js
@@ -1,1 +1,1 @@
-export default function addition (n1, n2) {return parseFloat(n1)/parseFloat(n2)};
+export default function divide (n1, n2) {return parseFloat(n1)/parseFloat(n2)};
--- src/functions/multiply.js
+++ src/functions/multiply.js
@@ -1,1 +1,1 @@
-export default function addition (n1, n2) {return parseFloat(n1)*parseFloat(n2)};
+export default function multiply (n1, n2) {return parseFloat(n1)*parseFloat(n2)};
